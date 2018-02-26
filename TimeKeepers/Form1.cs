206963b63038cd6d159c38f0469da4bed8881e5a
museypoo <kaydendeason@gmail.com>
2018-02-25 21:58:26 -0500

    Add project files.

---
+++ TimeKeepers/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace TimeKeepers
+{
+    public struct SystemTime
+    {
+        public ushort Year;
+        public ushort Month;
+        public ushort DayOfWeek;
+        public ushort Day;
+        public ushort Hour;
+        public ushort Minute; // here
+        public ushort Second;
+        public ushort Millisecond;
+    };
+
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", EntryPoint = "SetSystemTime", SetLastError = true)]
+        public extern static bool Win32SetSystemTime(ref SystemTime sysTime);
+
+        static System.Timers.Timer jew = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            jew.Interval = 1 * 60 * 1000;
+            jew.Elapsed += Jew_Elapsed;
+            jew.Start();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private static void Jew_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime ntptime = GetNetworkTime();
+            SystemTime win32time = new SystemTime();
+            win32time.Year = (ushort)ntptime.Year;
+            win32time.Month = (ushort)ntptime.Month;
+            win32time.DayOfWeek = (ushort)ntptime.DayOfWeek;
+            win32time.Day = (ushort)ntptime.Day;
+            win32time.Hour = (ushort)ntptime.Hour;
+            win32time.Minute = (ushort)ntptime.Minute;
+            win32time.Second = (ushort)ntptime.Second;
+            Win32SetSystemTime(ref win32time);
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            notifyIcon1.Visible = false;
+
+            Application.Exit();
+        }
+
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
+        }
+
+        public static DateTime GetNetworkTime()
+        {
+            //default Windows time server
+            const string ntpServer = "time.windows.com";
+
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
+
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            //The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            //NTP uses UDP
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+
+                //Stops code hang if NTP is blocked
+                socket.ReceiveTimeout = 3000;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
+            }
+
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            //**UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = Properties.Resources.qigtfoG;
+            notifyIcon1.ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Stop and exit", Exit) });
+            notifyIcon1.Text = "Time Keeper";
+            ShowInTaskbar = false;
+            Hide();
+        }
+    }
+}
